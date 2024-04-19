8e561739de81c0f836dac776de755e8a93ab8f3c
ZAYAN KHAN <145120648+zayankhan99@users.noreply.github.com>
2024-04-19 21:54:58 +0500

    .

---
+++ app.js
@@ -0,0 +1,89 @@
+// Chapter 13-15
+// Q-1
+
+// var studentName = [];
+// studentName = prompt("Please Enter Your Name");
+// document.write(studentName);
+
+// Q-3
+
+// var names = ['zayan','ali','ahmed','shamir','daniyal'];
+
+// Q-4
+
+//var arr = [10,11,12,15];
+
+// Q-5
+
+//var arr = [true,false];
+
+// Q-6
+
+// var arr = ['zayan',5,'true'];
+
+// Q-7
+
+// var  qualifications = ['SSC', 'HSC', 'BCS','BS', 'BCOM', 'MS', 'M.Phil', 'PhD'];
+// document.write(`<h1>Qualification</h1><br> 1)${arr [0]}
+// <br> 2)${arr [1]} <br> 3)${arr [2]} <br> 4)${arr [3]}<br> 5)${arr [4]}
+// <br> 6)${arr [5]} <br> 7)${arr [6]} <br> 8)${arr [7]}`)
+
+// Q-8
+
+// var studentName=['zayan','ali','wahaj'];
+// var score =[200,255,320];
+// var percentAge=[score[0]/500*100,score[1]/500*100,score[2]/500*100];
+// document.write(`Score of ${studentName[0]} is ${score[0]}. Percentage: ${percentAge[0]} % <br>`)
+// document.write(`Score of ${studentName[1]} is ${score[1]}. Percentage: ${percentAge[1]} % <br>`)
+// document.write(`Score of ${studentName[2]} is ${score[2]}. Percentage: ${percentAge[2]} % <br>`)
+
+// Q-9
+// (a)
+
+// var colorNAmes = ['red','blue','black'];
+
+// (b)
+
+// var colorNAmes = ['red','blue','black'];
+// var update = prompt('What color you want to add in the start');
+// colorNAmes.unshift(update);
+// document.write(colorNAmes);
+
+// (c)
+
+// var colorNAmes = ['red','blue','black'];
+// var color1 = prompt('What color you want to add in 0 index');
+// var color2 = prompt('What color you want to add in 1 index');
+// colorNAmes.unshift(color1,color2);
+// document.write(colorNAmes)
+
+// (d)
+
+// var color = ['red','blue','black'];
+// color.pop();
+// document.write(color)
+
+// (e)
+
+// var color = ['red','blue','black'];
+// color.pop();
+// document.write(color)
+
+// (f)
+
+// var color = ['gary', 'pink', 'white', 'black', 'yellow', 'green']
+// var index1 = prompt('at which index you wants to add a color')
+// var index2 = prompt('at ')
+// color.splice()
+
+
+// Q-10
+
+var studentScore = [120, 357, 235, 523, 243, 263];
+document.write(`${<h1>studentScore</h1>}`)
+
+// Q-11
+
+
+// studentScore.sort();
+// document.write(studentScore);
